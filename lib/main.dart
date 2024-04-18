d33e0ea005724c6cb728ece363c846aba18771fb
Levy Kariuki <levthigkari@gmail.com>
2024-04-18 16:09:01 +0100

    Pushing project online to Github

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:levexauto/views/splash/splash_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-//Git Push
+//Git Push test 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
